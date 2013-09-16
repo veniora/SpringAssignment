a980da61841abf62601af6a43cd255b4a3abe369
veniora <veniora7@gmail.com>
2013-09-16 20:47:07 +1200

    Added models and changed base context to one parent package. If it fails, maven clean then maven install to fix it

---
+++ src/main/java/com/springapp/mvc/model/LibraryModel.java
@@ -0,0 +1,34 @@
+package com.springapp.mvc.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "LIBRARIES")
+public class LibraryModel {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
